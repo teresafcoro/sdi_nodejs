ea5ec4130bdcd391093776e4686a0ba2247a0bd1
UO263728 <UO263728@uniovi.es>
2023-03-20 09:57:19 +0100

    SDI-2223-505-7.4-Recursos estáticos, peticiones POST y parámetros, Enrutamiento y comodines.

--- musicstoreapp/routes/songs.js
+++ musicstoreapp/routes/songs.js
@@ -22,4 +22,19 @@ module.exports = function (app) {
         let response = 'id: ' + req.params.id + '<br>' + 'Tipo de música: ' + req.params.kind;
         res.send(response);
     });
+
+    app.get('/promo*', function (req, res) {
+        res.send('Respuesta al patrón promo*');
+    });
+
+    app.get('/pro*ar', function (req, res) {
+        res.send('Respuesta al patrón pro*ar');
+    });
+
+    app.post('/songs/add', function (req, res) {
+        let response = "Canción agregada: " + req.body.title + "<br>"
+            + " genero: " + req.body.kind + "<br>"
+            + " precio: " + req.body.price;
+        res.send(response);
+    });
 };
