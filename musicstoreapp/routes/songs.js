79a951d88afb56c259b2b38c5e51927ba4a59316
UO263728 <UO263728@uniovi.es>
2023-03-22 11:33:06 +0100

    SDI-2223-505-8.2-Arquitectura para el acceso a datos.

--- musicstoreapp/routes/songs.js
+++ musicstoreapp/routes/songs.js
@@ -1,4 +1,4 @@
-module.exports = function (app, MongoClient) {
+module.exports = function (app, songsRepository) {
     app.get("/songs", function (req, res) {
         let songs = [{
             "title": "Blank space",
@@ -29,18 +29,11 @@ module.exports = function (app, MongoClient) {
 
     app.post('/songs/add', function (req, res) {
         let song = {title: req.body.title, kind: req.body.kind, price: req.body.price}
-        MongoClient.connect(app.get('connectionStrings'), function (err, dbClient) {
-            if (err) {
-                res.send("Error de conexión: " + err);
-            } else {
-                const database = dbClient.db("musicStore");
-                const collectionName = 'songs';
-                const songsCollection = database.collection(collectionName);
-                songsCollection.insertOne(song)
-                    .then(result => res.send("canción añadida id: " + result.insertedId))
-                    .then(() => dbClient.close())
-                    .catch(err => res.send("Error al insertar " + err));
-            }
+        songsRepository.insertSong(song, function (songId){
+           if (songId == null)
+               res.send("Error al insertar canción");
+           else
+               res.send("Agregada la canción ID: " + songId);
         });
     });
 
