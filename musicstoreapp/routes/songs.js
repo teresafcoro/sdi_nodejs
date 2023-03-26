f42f045fa672b7cd9c070314ebdbb954d961221f
UO263728 <UO263728@uniovi.es>
2023-03-26 18:37:50 +0200

    SDI-2223-505-8.4-Registro de usuario, autenticación y uso de sesión.

--- musicstoreapp/routes/songs.js
+++ musicstoreapp/routes/songs.js
@@ -25,11 +25,24 @@ module.exports = function (app, songsRepository) {
     });
 
     app.get('/songs/add', function (req, res) {
+        if (req.session.user == null) {
+            res.redirect("/shop");
+            return;
+        }
         res.render("songs/add.twig");
     });
 
     app.post('/songs/add', function (req, res) {
-        let song = {title: req.body.title, kind: req.body.kind, price: req.body.price};
+        if (req.session.user == null) {
+            res.redirect("/shop");
+            return;
+        }
+        let song = {
+            title: req.body.title,
+            kind: req.body.kind,
+            price: req.body.price,
+            author: req.session.user
+        };
         songsRepository.insertSong(song, function (songId) {
             if (songId == null)
                 res.send("Error al insertar canción");
